2af8ca5a4381458db4432c471644f88ab4196153
panayotsky.dev <panayotsky.dev@gmail.com>
2023-02-12 23:19:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div style={{maxWidth: '60rem',
-      margin:' 2rem auto',
-      padding: '1rem',
-      boxShadow: '0 2px 8px rgba(0, 0, 0, 0.25)',
-      borderRadius: '14px',
-      backgroundColor: 'white'}}>
+    <div style={{maxWidth: '60rem',margin:' 2rem auto',padding: '1rem',boxShadow: '0 2px 8px rgba(0, 0, 0, 0.25)',borderRadius: '14px',backgroundColor: 'white'}}>
       <div style={{ display: 'flex', justifyContent: 'space-between', margin: '1rem', paddingBottom: '1rem',  borderBottom: '1px solid #ccc' }}>
         <h3 style={{margin: '0 0 0.25rem 0'}}>Sushi</h3>
         <div style={{fontStyle: "italic"}}>I prefer steak than sushi.</div>
